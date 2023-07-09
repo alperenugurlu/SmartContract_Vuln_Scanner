50590c1a0c246b3b84797d0c297df7447314bc94
Alperen Ugurlu <64872731+alperenugurlu@users.noreply.github.com>
2023-07-09 18:06:00 +0300

    SmartContract_Vuln_Scanner.py

--- SmartContract_Vuln_Scanner.py
+++ SmartContract_Vuln_Scanner.py
@@ -1,5 +1,51 @@
 import requests
 
+# Color print functions
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+
+prCyan('''
+                                _                 ___  
+                               | |               / _ \ 
+  _ __ ___     ___  __      __ | |_  __      __ | | | -|
+ | '_ ` _ \   / _ \ \ \ /\ / / | __| \ \ /\ / / | | |----|
+ | | | | | | |  __/  \ V  V /  | |_   \ V  V /  | |_| --|
+ |_| |_| |_|  \___|   \_/\_/    \__|   \_/\_/    \___/ 
+                                                       
+                                                                                                                         
+
+				 _                 
+				//\lperen  |U|gurlu
+                   `-'     
+
+                      
+''' )
+
+prRed('''''''''''''''
+
+	     Smart Contract Vuln Scanner
+         *    *      * * *  *   *  
+        * *    ******          * * 
+           **         *   ** **   *
+                         *         
+                      *   ** **    
+        *     *                   *
+           *    *   *          *   
+          *            *    *   ** 
+               *   *               
+            *    *                 
+                                   
+         *        *  *             
+
+
+''''''''''''''')
+
 def check_contract_vulnerabilities():
     api_key = input("Etherscan API anahtarınızı girin: ")
     contract_address = input("Akıllı sözleşme adresini girin: ")
